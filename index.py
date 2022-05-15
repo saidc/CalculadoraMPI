7a7319ca3ba1ca8dd77a632e02f1720d5cb75055
saidjoc@gmail.com <saidjoc@gmail.com>
2022-05-14 19:18:59 -0500

    udpate

---
+++ index.py
@@ -0,0 +1,67 @@
+import time
+import multiprocessing
+import concurrent.futures 
+
+print("CALDULADODA POR MULTIPROCESAMIENTO ")
+print(  " El usuario debe ingresar dos numeros por \n"
+        " por la consola y el programa realizara el calculo de \n"
+        " Suma , Resta, Multiplicacion, y Division ( el segundo numero no puede ser CERO ) ")
+
+numInputCount = 0
+number1 = None
+number2 = None
+errorcount = 0
+while( numInputCount < 2 and errorcount < 10 ):
+    if(numInputCount == 0):
+        print("\n\nINGRESE EL 1ER NUMERO")
+        try:
+            number1 = int(input())
+            numInputCount += 1
+        except:
+            number1 = None
+            print("Debe digitar un numero valido ")
+            errorcount += 1
+    elif(numInputCount == 1):
+        print("\n\nINGRESE EL 2do NUMERO")
+        error = False
+        try:
+            number2 = int(input())
+            error = number2 == 0
+        except:
+            error = True
+        if(error):
+            number2 = None
+            print("Debe digitar un numero valido distinto de Cero")
+            errorcount += 1
+        else:
+            numInputCount += 1
+print(( " Se agregaron los dos numeros " if(numInputCount >= 2 and errorcount < 10) else " No se agregaron los numeros por error"))
+
+def suma(a,b):
+    return a+b
+def resta(a,b):
+    return a-b
+def multiplicacion(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+
+if(numInputCount >= 2 and errorcount < 10):
+    print("Numero 1: ", number1)
+    print("Numero 2: ", number2)
+    print("Inicia procesamiento ")
+
+    #sum = suma(number1,number2)
+    #multiprocessing.Process(target=suma,args=[number1,number2])
+    with concurrent.futures. ProcessPoolExecutor () as executor :
+        f1 = executor.submit(suma, number1,number2)
+        f2 = executor.submit(resta, number1,number2)
+    #res = resta(number1,number2)
+    #mul = multiplicacion(number1,number2)
+    #div = division(number1,number2)
+
+    print("resultado: \n    Suma: ", sum,"\n    Resta:",res,"\n Multiplicacion: ",mul,"\n   Division:",div)
+
+    start  = time.perf_counter()
+    finish = time.perf_counter()
+    print("Tiempo de Finalizado ", str( finish-start ))
